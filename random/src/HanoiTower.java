165fa26975620638b3386d98174721e7b83ce1cf
rafaelbcastilhos <rafaelbcastilhos@gmail.com>
2020-08-16 09:39:25 -0300

    add Hanoi

--- random/src/HanoiTower.java
+++ random/src/HanoiTower.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class HanoiTower {
+public class HanoiTower {
+    public static void main(String [] args){
+        Hanoi(0, 2, 1, 8);
+    }
+
+    public static void Hanoi(int ori, int dst, int aux, int qnt){
+        if(qnt == 1){
+            System.out.println("Move from " + ori + " to " + dst);
+        }else{
+            Hanoi(ori, aux, dst, qnt-1);
+            Hanoi(ori, dst, aux, 1);
+            Hanoi(aux, dst, ori, qnt-1);
+        }
+    }
 }
